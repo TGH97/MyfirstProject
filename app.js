9d121f82f0524c526a6b6389e3c1be5a95137d38
TGH97 <t.3.g.h@hotmail.com>
2022-09-24 21:09:51 +0200

    just testing 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const data = require('./data.js')
 
 const app = express()
 app.engine('hbs', expressHandlebars.engine({
@@ -8,6 +9,15 @@ app.engine('hbs', expressHandlebars.engine({
 app.get('/', function(request, response){
     response.render('start.hbs')
 }) 
+
+app.get('/movies', function(request, response){
+    const model = {
+        movies: data.movies
+    }
+
+    response.render('movies.hbs', model)
+}) 
+
 
 app.use(
     express.static('public')
---
+++ data.js
@@ -0,0 +1,13 @@
+exports.movies= [{
+    id: 1,
+    title: "GoldenEye",
+    grade: 9
+}, {
+    id: 2,
+    title: "GoldenNose",
+    grade: 4 
+},{
+    id: 3,
+    title: "GoldenEir",
+    grade: 7 
+}]
